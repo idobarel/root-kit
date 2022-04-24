e7c003639dce7922fc60c3559dac1125f7bee163
Ido Barel <55856610+idobarel@users.noreply.github.com>
2022-04-25 01:50:22 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,10 @@ echo "GodZilo's Root-Kit Installation:"
 echo "Running..."
 sudo pip3 install -r requirements.txt
 cp main.py rootkit
+sudo cp -r www /usr/local/bin/
 chmod +x rootkit
 sudo cp rootkit /usr/local/bin/
 clear
 rm rootkit
 echo "Installed!"
+echo "Use rootkit -h"
